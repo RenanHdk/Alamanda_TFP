db14c4ed6df6c47e0b945249f9d591358fe6d07d
Renan <114091580+RenanHdk@users.noreply.github.com>
2023-11-13 14:29:14 -0300

    Delete functions directory

--- functions/teste.py
+++
@@ -1,8 +0,0 @@
-from app import inputM
-from functions.twilio import send_message
-
-def teste(sender_id):
-    msg = inputM(sender_id, "1 ou 2 ?")
-    while(msg!=1 or msg !=2):
-        msg = inputM(sender_id, "Entrada inválida, 1 ou 2 APENAS")
-    send_message(msg)
