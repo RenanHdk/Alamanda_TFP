9f08a5239840b294f14d1baa253012db8cb1b3d4
Renan <114091580+RenanHdk@users.noreply.github.com>
2023-11-13 14:48:19 -0300

    Update teste.py

--- functions/teste.py
+++ functions/teste.py
@@ -3,7 +3,8 @@ from functions.twilio import send_message
 
 def entrada(sender_id, txt):
     send_message(sender_id, txt)
-    return request.form['Body']
+    msg = request.form.get('Body', '').strip()
+    return msg
 
 def teste(sender_id):
     msg = entrada(sender_id, "1 ou 2")
